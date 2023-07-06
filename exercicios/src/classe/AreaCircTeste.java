094dbc34af4e5567b7a9144b401ca4efbb35a23f
CocoaSweet <li.piva@hotmail.com>
2023-07-06 20:30:43 -0300

    Area circunferencia com método, constructor e variavel estatica

---
+++ exercicios/src/classe/AreaCircTeste.java
@@ -0,0 +1,12 @@
+package classe;
+
+public class AreaCircTeste {
+	public static void main(String[] args) {
+		
+		AreaCirc area = new AreaCirc(5);
+         
+		System.out.println(area.area());
+		System.out.println(AreaCirc.area(4));
+	}
+	
+}
