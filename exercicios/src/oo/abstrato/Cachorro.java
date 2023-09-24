c0427a8991665325bedec0dcdc23ae0eabb23921
CocoaSweet <li.piva@hotmail.com>
2023-09-24 20:25:19 -0300

    abstract

---
+++ exercicios/src/oo/abstrato/Cachorro.java
@@ -0,0 +1,15 @@
+package oo.abstrato;
+
+public class Cachorro extends Mamifero{
+
+	@Override
+	public String mamar() {
+		return "Vai mamar leite";
+	}
+
+	@Override
+	public String mover() {
+		return "Com as patas";
+	}
+
+}
